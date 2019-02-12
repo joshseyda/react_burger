4c02d316e51aabb73442365d7cd080cecb7183a6
joshseyda <josh.seyda@gmail.com>
2019-02-12 14:20:57 -0500

    Fixed CSS styling on Order Button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,22 @@ class BurgerBuilder extends Component{
        cheese: 0,
        meat: 0
      },
-     totalPrice: 4
+     totalPrice: 4,
+     purchasable: false
+  }
+
+  updatePurchasedState  () {
+    const ingredients = {
+      ...this.state.ingredients
+    };
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el)=>{
+        return sum + el;
+      }, 0);
+    this.setState({purchasable: sum > 0}); 
   }
 
   addIngredientHandler = (type) => {
@@ -64,6 +79,7 @@ class BurgerBuilder extends Component{
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice}/>
       </Aux>
     );
