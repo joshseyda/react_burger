d56c9a0f25bd4842326cecadd5cf6085cfccaf14
joshseyda <josh.seyda@gmail.com>
2019-02-20 17:26:33 -0500

    Sidedrawer Slides out onClick of backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,13 +6,17 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (props.open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
   return(
     <Aux>
       <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={styles.SideDrawer} >
-      <div className={styles.Logo}>
-        <Logo />
-      </div>
+      <div className={attachedClasses.join(' ')}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
         <nav>
           <NavigationItems />
         </nav>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import navigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
